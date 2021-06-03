169af9c75a9af1e1d058d42308f36ee6872de24c
hejunjie <hejunjie@yodinfo.com>
2021-06-03 11:48:56 +0800

    feature:二叉搜索树前序遍历

--- data-structure/src/BinarySearchTree.java
+++ data-structure/src/BinarySearchTree.java
@@ -77,12 +77,66 @@ public class BinarySearchTree<E extends Comparable<E>> {
         return node;
     }
 
+    public boolean contains(E e) {
+        // 简单判断 应该用工具类
+        if (Objects.isNull(e)) {
+            return false;
+        }
+
+        return contains(root, e);
+    }
+
+    private boolean contains(Node node, E e) {
+        if (node == null) {
+            return false;
+        }
+        if (e.compareTo(node.e) == 0) {
+            return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else // (e.compareTo(node.e) > 0)
+        {
+            return contains(node.right, e);
+        }
+    }
+
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println("Node e value is " + node.e);
+
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
     @Override
     public String toString() {
-        return "BinarySearchTree{" +
-                "root=" + root +
-                ", size=" + size +
-                '}';
+        StringBuilder res = new StringBuilder();
+        generateBSTStr(root, 0, res);
+        return res.toString();
+    }
+
+    private void generateBSTStr(Node node, int depth, StringBuilder res) {
+        if (node == null) {
+            res.append(generateDepthStr(depth) + "null \n");
+            return;
+        }
+        res.append(generateDepthStr(depth) + node.e + "\n");
+        generateBSTStr(node.left, depth + 1, res);
+        generateBSTStr(node.right, depth + 1, res);
+    }
+
+    private String generateDepthStr(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            res.append("--");
+        }
+        return res.toString();
     }
 
     public static void main(String[] args) {
@@ -98,6 +152,14 @@ public class BinarySearchTree<E extends Comparable<E>> {
         searchTree.add(9);
         searchTree.add(7);
         searchTree.add(null);
+        System.out.println(searchTree);
+        System.out.println("Test contains method");
+        System.out.println(searchTree.contains(11));
+        System.out.println(searchTree.contains(22));
+        System.out.println(searchTree.contains(9));
+        System.out.println(searchTree.contains(7));
+        System.out.println(searchTree.contains(null));
+//        searchTree.preOrder();
         System.out.println(searchTree);
     }
 }
